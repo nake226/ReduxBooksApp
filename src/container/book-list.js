8dc6e1d1802758e14539d6378cfe5b29f743cdf5
中井　貴之 <uu135923@CA3919.local>
2018-11-01 14:51:06 +0900

    add: viewとstateと繋ぐ処理(mapStateToPropsとconnect) finish: lesson44

--- src/container/book-list.js
+++ src/container/book-list.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 /**
  * コンテナ：reduxが管理するstateに直接アクセスできるcomponentのこと
  * コンテナの基準：data（state）をcareするcomponentかどうか
  * → booklistは書籍リストという状態に関与するのでコンテナ。
  */
-export default class BookList extends Component {
+class BookList extends Component {
   // 受け取った書籍リストをレンダリング
   renderList() {
     return this.props.books.map((book) => {
@@ -25,3 +25,18 @@ export default class BookList extends Component {
     )
   }
 }
+
+/**
+ * @param {*} state 
+ * stateの受け渡し
+ * keyの'books': BookListのprops.booksと同じ
+ * valueの'books': reducers/index.jsのrootReducerで定義されているbooks
+ */
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  }
+}
+
+// view(BookListクラス)とdata（state受け渡し）のコネクト
+export default connect(mapStateToProps)(BookList);
